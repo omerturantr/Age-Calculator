459977d1f022a78bb2939b1b29da27880a7e7eea
omerturan <omerturanpersonal@gmail.com>
2025-05-24 02:53:06 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Ömer"
+birth_year = 2003
+age = 2025 - birth_year
+
+print("What's your name?")
+print(name)
+
+print("What year were you born?")
+print(age)
+
+print("Hello, " + name + "! You are " + str(age) + " years old.")
